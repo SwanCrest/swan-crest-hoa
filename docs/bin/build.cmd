0e2beb38d5428734f540be3fc7710d24593fd66a
Kyle Rader <kylewrader@gmail.com>
2022-01-29 13:49:38 -0800

    Capitilize CC&R doc names

--- docs/bin/build.cmd
+++ docs/bin/build.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 
 rem Word
-pandoc -o "cc&r.docx" -f markdown --reference-doc reference.docx --toc ccnr.md
+pandoc -o "CC&R.docx" -f markdown --reference-doc reference.docx --toc ccnr.md
 pandoc -o bylaws.docx -f markdown --reference-doc reference.docx --toc bylaws.md
 pandoc -o articles_of_incorporation.docx -f markdown --reference-doc reference.docx --toc articles_of_incorporation.md
 
 rem PDF
-pandoc -o "cc&r.pdf" -f markdown --toc -V geometry:"top=1.04in, bottom=1.71in, left=1.07in, right=1.06in" ccnr.md
+pandoc -o "CC&R.pdf" -f markdown --toc -V geometry:"top=1.04in, bottom=1.71in, left=1.07in, right=1.06in" ccnr.md
 pandoc -o bylaws.pdf -f markdown --toc -V geometry:"top=1.04in, bottom=1.71in, left=1.07in, right=1.06in" bylaws.md
 pandoc -o articles_of_incorporation.pdf -f markdown --toc -V geometry:"top=1.04in, bottom=1.71in, left=1.07in, right=1.06in" articles_of_incorporation.md
